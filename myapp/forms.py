a2334c47034cef7ec1e42beb82f3eaa30d7be78a
Stealthhh <67944328+Stealthhh@users.noreply.github.com>
2022-01-29 02:12:07 +0200

    Delete myapp directory

--- myapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-
-from .models import MyUser
-
-
-class SignUpForm(UserCreationForm):
-    class Meta:
-        model = MyUser
-        fields = ('username', 'password1', 'password2')
